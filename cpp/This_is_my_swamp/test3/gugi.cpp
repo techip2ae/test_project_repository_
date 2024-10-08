0c9cf14489c888d2f457b7ced137c8082fea3898
Velainet <egorchekov2008@gmail.com>
2024-10-08 19:32:18 +0300

    Сделал "рандомные"числа для кода с матрицей

--- cpp/This_is_my_swamp/test3/gugi.cpp
+++ cpp/This_is_my_swamp/test3/gugi.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
     
 int main()
 {
+    srand(time(NULL));
     int max_mass;
     cin >> max_mass;
     int mass[max_mass];
-    
     
     for(int i=1 ; i < max_mass; i++)
     {
         mass[i] = rand() % 50;
     }
 
-    for(int i=1 ; i < max_mass ; i++)
+    for(int i=1 ; i <= max_mass;i++)
     {    
         cout << "mass[" << i << "]=" << mass[i] << endl;
     }
