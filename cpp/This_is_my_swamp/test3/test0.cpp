a01f57f8761e05ee0cba5ff8cb20359e35dfc2b1
Velainet <egorchekov2008@gmail.com>
2024-10-09 11:49:56 +0300

    OOP

---
+++ cpp/This_is_my_swamp/test3/test0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class goga
+{
+public:
+    int size;
+    int go;
+};
+
+int main()
+{
+    goga kicha;
+    int tyt;
+    cin >> tyt;
+
+    kicha.size = tyt;
+
+    cout << kicha.size;
+}
