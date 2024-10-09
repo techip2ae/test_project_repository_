2508e564128fc246aabca4624019e682aefbc2c8
Velainet <egorchekov2008@gmail.com>
2024-10-09 11:27:49 +0300

    nan

--- cpp/This_is_my_swamp/test3/gugi.cpp
+++ cpp/This_is_my_swamp/test3/gugi.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
-#include <time.h>
+#include <iostream> 
+
 using namespace std;
     
 int main()
 {
-    srand(time(NULL));
     int max_mass;
     cin >> max_mass;
     int mass[max_mass];
     
-    for(int i=1 ; i < max_mass; i++)
-    {
-        mass[i] = rand() % 50;
+    for(int i=1 ; i <= max_mass; i++) {
+    
+    cout << "write number";
+    cin >> mass[i];
+    
     }
-
-    for(int i=1 ; i <= max_mass;i++)
-    {    
+    for(int i=1 ; i <= max_mass; i++){
         cout << "mass[" << i << "]=" << mass[i] << endl;
     }
-
-}
+}    
