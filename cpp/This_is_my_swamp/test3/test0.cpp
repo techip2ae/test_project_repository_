b8db63bd3633a5f01b9577aa1fdf64c4714fba5f
Velainet <egorchekov2008@gmail.com>
2024-10-10 11:32:28 +0300

    New scrpipt

---
+++ cpp/This_is_my_swamp/test3/test0.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+  
+using namespace std;
+ 
+int main()
+{
+    char op = '*';
+    int n = 10;
+    switch(int k{2}; op)
+    {
+        case '+':
+            cout << n + k << endl;
+            break;
+        case '-':
+            cout << n - k << endl;
+            break;
+        case '*':
+            cout << n * k << endl;
+            break;
+    }
+}
